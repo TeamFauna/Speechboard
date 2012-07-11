3356f30d0cdcb6052911a5273a6ae38ae04860f3
Will Hughes <willhughes01@gmail.com>
2012-07-11 13:21:15 -0400

    Added old speech to text stuff (deprecated (most likely))

--- lib/private/stt/curllibackup.cc
+++ lib/private/stt/curllibackup.cc
@@ -1,32 +1,32 @@
-#include <stdio.h>
-#include <string.h>
-//#include <string>
-#include <stdlib.h>
+#include <cstdio>
+#include <cstring>
+#include <string>
+#include <cstdlib>
  
-#include <curl/curl.h>
 
-//using std::string;
+using std::string;
 
-size_t callback(void *ptr, size_t size, size_t count, void *stream) {
-    printf("THIS IS THE RESPONSE\n");
-    ptr->:
-    return size*count;
+string* callback(void *ptr, size_t size, size_t count, void *stream) {
+    return (string*)stream;
 }
 
-int speech_to_text(char* audio, size_t audiolength) {
+string speech_to_text(char* audio, size_t audiolength) {
   CURL *curl;
   CURLcode res;
-  int* response;
+  string response;
  
   struct curl_slist *headerlist=NULL;
   
   curl_global_init(CURL_GLOBAL_ALL);
  
   curl = curl_easy_init();
+  /* initalize custom header list (stating that Expect: 100-continue is not
+     wanted */ 
   headerlist = curl_slist_append(headerlist, "Expect:");
   headerlist = curl_slist_append(headerlist, "Content-Type: audio/x-flac; rate=16000");
   printf("%s\n", headerlist->data);
   if(curl) {
+    /* what URL that receives this POST */ 
     curl_easy_setopt(curl, CURLOPT_URL, "https://www.google.com/speech-api/v1/recognize?xjerr=1&client=chromium&lang=en-US");
 
     curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headerlist);
@@ -42,10 +42,12 @@ int speech_to_text(char* audio, size_t audiolength) {
     res = curl_easy_perform(curl);
 
     printf("Response\n");
-    printf("%d\n", (*response));
+    printf("%s\n", response.c_str());
  
+    /* always cleanup */ 
     curl_easy_cleanup(curl);
  
+    /* free slist */ 
     curl_slist_free_all (headerlist);
   }
   return 0;
