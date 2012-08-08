5ee1be7d14037d103696ed6ec67a84b0ecb91a0c
Andrew Russell <deadhead.russell@gmail.com>
2012-08-08 18:52:08 -0400

    updating...

--- spchbrd/js/main.js
+++ spchbrd/js/main.js
@@ -1,10 +1,6 @@
 function init() {
-  var inputDiv = document.createElement('Div');
-  document.body.appendChild(inputDiv);
-
   var pollingDiv = document.createElement('Div');
   document.body.appendChild(pollingDiv);
 
-  startInput(inputDiv);
   startPolling(pollingDiv);
 }
--- spchbrd/js/polling.js
+++ spchbrd/js/polling.js
@@ -15,9 +15,37 @@ function Poller(interval, callback) {
   this.poll = poll;
 }
 
+var emailToImg = {
+  'deadhead.russell@gmail.com': 'russell',
+  'zetalmac@gmail.com': 'noah',
+  'willhughes01@gmail.com': 'will',
+  'munnand@gmail.com': 'munn'
+};
+
+function GetPicture(speaker) {
+  
+}
+
+function CreateTextBox(speaker, text) {
+  var boxDiv = document.createElement('Div');
+
+  var speakerSpan = document.createElement('Span');
+  speakerSpan.style.fontWeight = 'bold';
+  speakerSpan.appendChild(document.createTextNode(speaker + ':'));
+  boxDiv.appendChild(speakerSpan);
+
+  var textSpan = document.createElement('Span');
+  textSpan.appendChild(document.createTextNode(text));
+  boxDiv.appendChild(textSpan);
+
+  return boxDiv;
+}
+
 function startPolling(div) {
   function display(json) {
-    div.innerHTML += json + '<br>';
+    var speaker = json.speaker;
+    var text = json.text;
+    div.appendChild(CreateTextBox(speaker, text));
   }
 
   var poller = new Poller(1000, display);
